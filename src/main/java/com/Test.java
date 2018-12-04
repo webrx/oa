9ea872553f6874f1ba0fb918674f4901bce88dca
webrx <webrx@126.com>
2018-12-04 17:10:17 +0800

    修改了Test.java 增加了几行

--- src/main/java/com/Test.java
+++ src/main/java/com/Test.java
@@ -6,5 +6,12 @@ package com;
 public class Test {
     public static void main(String[] args) {
         System.out.println(2*3);
+        System.out.println(2*3);
+        System.out.println(2*3);
+        System.out.println(2*3);
+        System.out.println(2*3);
+        System.out.println(2*3);
+        System.out.println(2*3);
+        System.out.println(2*3);
     }
 }
